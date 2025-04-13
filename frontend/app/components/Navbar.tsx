21422ded56dec75158d7be017f8c392389dafd76
Colin Henderson <colin@cma-100-64-24-76.resnet.ucsc.edu>
2025-04-13 05:46:51 -0700

    minor ui tweaking

--- frontend/app/components/Navbar.tsx
+++ frontend/app/components/Navbar.tsx
@@ -15,30 +15,33 @@ interface NavbarProps {
 
 export default function Navbar({ routes }: NavbarProps) {
   return (
-    <nav className={styles.navbar}>
-      <ul className={styles.navList}>
-        {routes.map((route, index) => (
-          <li key={index} className={styles.navItem}>
-            <Link
-              href={route.path}
-              className={
-                route.isMain
-                  ? `${styles.navLink} ${styles.mainNavLink}`
-                  : styles.navLink
-              }
-            >
-              {route.icon && (
-                <img
-                  src={route.icon}
-                  alt={`${route.name} icon`}
-                  className={styles.navIcon}
-                />
-              )}
-              {route.name}
-            </Link>
-          </li>
-        ))}
-      </ul>
-    </nav>
+    <div className={styles.navbarContainer}>
+      <div className={styles.topRightText}>Verity</div>
+      <nav className={styles.navbar}>
+        <ul className={styles.navList}>
+          {routes.map((route, index) => (
+            <li key={index} className={styles.navItem}>
+              <Link
+                href={route.path}
+                className={
+                  route.isMain
+                    ? `${styles.navLink} ${styles.mainNavLink}`
+                    : styles.navLink
+                }
+              >
+                {route.icon && (
+                  <img
+                    src={route.icon}
+                    alt={`${route.name} icon`}
+                    className={styles.navIcon}
+                  />
+                )}
+                {route.name}
+              </Link>
+            </li>
+          ))}
+        </ul>
+      </nav>
+    </div>
   );
 }
